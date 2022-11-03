e4760443fc3f094e8681d703a18ea958296f97f0
oteto <etochan5@gmail.com>
2022-11-03 23:15:39 +0900

    answer 6

---
+++ src/_6_medium_zig_zag_conversion.rs
@@ -0,0 +1,66 @@
+use core::num;
+
+/*
+ * @lc app=leetcode id=6 lang=rust
+ *
+ * [6] ZigZag Conversion
+ */
+struct Solution {}
+
+// @lc code=start
+impl Solution {
+    pub fn convert(s: String, num_rows: i32) -> String {
+        if num_rows == 1 {
+            return s;
+        }
+        let chars = s.chars().collect::<Vec<char>>();
+        let mut ret = String::with_capacity(chars.len());
+
+        let m = 2 * (num_rows - 1);
+        let mut p = 2 * (num_rows - 1);
+        let mut index = 0;
+        let mut row = 0;
+        let mut f = true;
+
+        while 0 <= p {
+            match chars.get(index) {
+                Some(&c) => {
+                    ret.push(c);
+                    if m == p || p == 0 {
+                        index += m as usize;
+                    } else {
+                        match f {
+                            true => {
+                                index += p as usize;
+                                f = !f;
+                            }
+                            false => {
+                                index += (m - p) as usize;
+                                f = !f;
+                            }
+                        }
+                    }
+                }
+                None => {
+                    p -= 2;
+                    row += 1;
+                    index = row;
+                    f = true;
+                }
+            }
+        }
+
+        ret
+    }
+}
+// @lc code=end
+
+#[test]
+fn test6() {
+    assert_eq!(
+        Solution::convert(String::from("PAYPALISHIRING"), 4),
+        String::from("PINALSIGYAHRPI")
+    );
+
+    assert_eq!(Solution::convert(String::from("A"), 1), String::from("A"));
+}
